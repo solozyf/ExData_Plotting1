11290b0511ad07a3e5f7da52bcedc2495abc94d3
solozyf <solozyf@hotmail.com>
2019-01-08 13:30:26 -0500

    First Commit

---
+++ Plot1.R
@@ -0,0 +1,10 @@
+
+rm(list=ls())
+txt<-read.table("household_power_consumption.txt",header = TRUE, sep = ";",dec=".")
+str(txt)
+txt$Date<-as.Date(txt$Date,"%d/%m/%Y")
+newdt<-subset(txt,Date>="2007-02-01"& Date<="2007-02-02")
+newdt$Global_active_power<-as.numeric(newdt$Global_active_power)
+png("plot1.png",width=480,height=480,units='px')
+hist((newdt$Global_active_power)/500,col='red',xlab="Global Active Power(kilowatts)",main ="Global Active Power")
+dev.off()
